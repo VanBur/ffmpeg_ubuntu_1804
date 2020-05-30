9bd07180e18eb4ce47cc937faad1119857edcdb4
ivanburcev <ivan@kinescope.io>
2020-05-30 17:56:00 +0300

    first commit

---
+++ ffmpeg.sh
@@ -0,0 +1,239 @@
+#!/bin/sh
+set -e
+
+UBUNTU_VERSION="1804"
+NASM_VERSION="2.14rc15"
+YASM_VERSION="1.3.0"
+LAME_VERSION="3.100"
+OPUS_VERSION="1.3.1"
+LASS_VERSION="0.14.0"
+FONT_CONFIG_VERSION="2.13.92"
+CUDA_VERSION="10.2.89-1"
+CUDA_REPO_KEY="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu$UBUNTU_VERSION/x86_64/7fa2af80.pub"
+CUDA_DIR="/usr/local/cuda"
+WORK_DIR="$HOME/ffmpeg-build-static-sources"
+DEST_DIR="$HOME/ffmpeg-build-static-binaries"
+
+mkdir -p "$WORK_DIR" "$DEST_DIR" "$DEST_DIR/bin"
+
+export PATH="$DEST_DIR/bin:$PATH"
+
+MYDIR="$(cd "$(dirname "$0")" && pwd)"
+
+Wget() { wget -cN "$@"; }
+
+Make() { make -j$(nproc); make "$@"; }
+
+Clone() {
+    local DIR="$(basename "$1" .git)"
+
+    cd "$WORK_DIR/"
+    test -d "$DIR/.git" || git clone --depth=1 "$@"
+
+    cd "$DIR"
+    git pull
+}
+
+installAptLibs() {
+    local PKGS="autoconf automake libtool patch make cmake bzip2 unzip wget git mercurial"
+    sudo apt-get update
+    sudo apt-get -y install $PKGS \
+      build-essential pkg-config texi2html software-properties-common \
+      libfreetype6-dev libgpac-dev libsdl1.2-dev libtheora-dev libva-dev \
+      libvdpau-dev libvorbis-dev libxcb1-dev libxcb-shm0-dev libxcb-xfixes0-dev zlib1g-dev libfribidi-dev gperf \
+      pkg-config libasound2-dev libssl-dev libexpat1-dev libxcb-composite0-dev
+}
+
+# Installing CUDA and the latest driver repositories from repositories
+installCUDASDK() {  
+    cd "$WORK_DIR/"
+    . /etc/os-release
+    local CUDA_REPO_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu${UBUNTU_VERSION}/x86_64/cuda-repo-ubuntu1804_${CUDA_VERSION}_amd64.deb"
+    Wget "$CUDA_REPO_URL"
+    sudo dpkg -i "$(basename "$CUDA_REPO_URL")"
+    sudo apt-key adv --fetch-keys "$CUDA_REPO_KEY"
+    sudo apt-get -y update
+    sudo apt-get -y install cuda
+
+    sudo env LC_ALL=C.UTF-8 add-apt-repository -y ppa:graphics-drivers/ppa
+    sudo apt-get -y update
+    sudo apt-get -y upgrade
+}
+
+# Installing the nVidia NVENC SDK.
+installNvidiaSDK() {
+    Clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+    make
+    make install PREFIX="$DEST_DIR"
+    patch --force -d "$DEST_DIR" -p1 < "$MYDIR/dynlink_cuda.h.patch" ||
+        echo "..SKIP PATCH, POSSIBLY NOT NEEDED. CONTINUED.."
+}
+
+compileFontConf() {
+    cd "$WORK_DIR/"
+    Wget "https://www.freedesktop.org/software/fontconfig/release/fontconfig-$FONT_CONFIG_VERSION.tar.gz"
+    tar xzvf "fontconfig-$FONT_CONFIG_VERSION.tar.gz"
+    cd "fontconfig-$FONT_CONFIG_VERSION"
+    ./configure --prefix=/usr        \
+            --sysconfdir=/etc    \
+            --localstatedir=/var \
+            --disable-docs       \
+            --docdir=/usr/share/doc/fontconfig-$FONT_CONFIG_VERSION && \
+    make install
+}
+
+compileNasm() {
+    cd "$WORK_DIR/"
+    Wget "http://www.nasm.us/pub/nasm/releasebuilds/$NASM_VERSION/nasm-$NASM_VERSION.tar.gz"
+    tar xzvf "nasm-$NASM_VERSION.tar.gz"
+    cd "nasm-$NASM_VERSION"
+    ./configure --prefix="$DEST_DIR" --bindir="$DEST_DIR/bin"
+    Make install distclean
+}
+
+compileYasm() {
+    cd "$WORK_DIR/"
+    Wget "http://www.tortall.net/projects/yasm/releases/yasm-$YASM_VERSION.tar.gz"
+    tar xzvf "yasm-$YASM_VERSION.tar.gz"
+    cd "yasm-$YASM_VERSION/"
+    ./configure --prefix="$DEST_DIR" --bindir="$DEST_DIR/bin"
+    Make install distclean
+}
+
+compileLibX264() {
+    cd "$WORK_DIR/"
+    Wget http://download.videolan.org/pub/x264/snapshots/x264-snapshot-20191217-2245-stable.tar.bz2
+    rm -rf x264-snapshot*/ || :
+    tar xjvf x264-snapshot-20191217-2245-stable.tar.bz2
+    cd x264-snapshot*
+    ./configure --prefix="$DEST_DIR" --bindir="$DEST_DIR/bin" --enable-static --enable-pic
+    Make install distclean
+}
+
+compileLibX265() {
+    if cd "$WORK_DIR/x265/"; then
+        hg pull
+        hg update
+    else
+        cd "$WORK_DIR/"
+        hg clone https://bitbucket.org/multicoreware/x265
+    fi
+
+    cd "$WORK_DIR/x265/build/linux/"
+    cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$DEST_DIR" -DENABLE_SHARED:bool=off ../../source
+    Make install
+
+    # forward declaration should not be used without struct keyword!
+    sed -i.orig -e 's,^ *x265_param\* zoneParam,struct x265_param* zoneParam,' "$DEST_DIR/include/x265.h"
+}
+
+compileLibAom() {
+    Clone https://aomedia.googlesource.com/aom
+    mkdir ../aom_build
+    cd ../aom_build
+    which cmake3 && PROG=cmake3 || PROG=cmake
+    $PROG -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$DEST_DIR" -DENABLE_SHARED=off -DENABLE_NASM=on ../aom
+    Make install
+}
+
+compileLibfdkcc() {
+    cd "$WORK_DIR/"
+    Wget -O fdk-aac.zip https://github.com/mstorsjo/fdk-aac/zipball/master
+    unzip -o fdk-aac.zip
+    cd mstorsjo-fdk-aac*
+    autoreconf -fiv
+    ./configure --prefix="$DEST_DIR" --disable-shared
+    Make install distclean
+}
+
+compileLibMP3Lame() {
+    cd "$WORK_DIR/"
+    Wget "http://downloads.sourceforge.net/project/lame/lame/$LAME_VERSION/lame-$LAME_VERSION.tar.gz"
+    tar xzvf "lame-$LAME_VERSION.tar.gz"
+    cd "lame-$LAME_VERSION"
+    ./configure --prefix="$DEST_DIR" --enable-nasm --disable-shared
+    Make install distclean
+}
+
+compileLibOpus() {
+    cd "$WORK_DIR/"
+    Wget "http://downloads.xiph.org/releases/opus/opus-$OPUS_VERSION.tar.gz"
+    tar xzvf "opus-$OPUS_VERSION.tar.gz"
+    cd "opus-$OPUS_VERSION"
+    ./configure --prefix="$DEST_DIR" --disable-shared
+    Make install distclean
+}
+
+compileLibVpx() {
+    Clone https://chromium.googlesource.com/webm/libvpx
+    ./configure --prefix="$DEST_DIR" --disable-examples --enable-runtime-cpu-detect --enable-vp9 --enable-vp8 \
+    --enable-postproc --enable-vp9-postproc --enable-multi-res-encoding --enable-webm-io --enable-better-hw-compatibility \
+    --enable-vp9-highbitdepth --enable-onthefly-bitpacking --enable-realtime-only \
+    --cpu=native --as=nasm --disable-docs
+    Make install clean
+}
+
+compileLibAss() {
+    cd "$WORK_DIR/"
+    Wget "https://github.com/libass/libass/releases/download/$LASS_VERSION/libass-$LASS_VERSION.tar.xz"
+    tar Jxvf "libass-$LASS_VERSION.tar.xz"
+    cd "libass-$LASS_VERSION"
+    autoreconf -fiv
+    ./configure --prefix="$DEST_DIR" --disable-shared
+    Make install distclean
+}
+
+compileFfmpeg(){
+    Clone https://github.com/FFmpeg/FFmpeg -b master
+
+    export PATH="$CUDA_DIR/bin:$PATH"  # ..path to nvcc
+    PKG_CONFIG_PATH="$DEST_DIR/lib/pkgconfig:$DEST_DIR/lib64/pkgconfig" \
+    ./configure \
+      --pkg-config-flags="--static" \
+      --prefix="$DEST_DIR" \
+      --bindir="$DEST_DIR/bin" \
+      --extra-cflags="-I $DEST_DIR/include -I $CUDA_DIR/include/" \
+      --extra-ldflags="-L $DEST_DIR/lib -L $CUDA_DIR/lib64/" \
+      --extra-libs="-lpthread" \
+      --enable-cuda \
+      --enable-cuda-sdk \
+      --enable-cuvid \
+      --enable-libnpp \
+      --enable-gpl \
+      --enable-libass \
+      --enable-libfdk-aac \
+      --enable-vaapi \
+      --enable-libfreetype \
+      --enable-libmp3lame \
+      --enable-libopus \
+      --enable-libtheora \
+      --enable-libvorbis \
+      --enable-libvpx \
+      --enable-libx264 \
+      --enable-libx265 \
+      --enable-nonfree \
+      --enable-libaom \
+      --enable-openssl \
+      --enable-nvenc
+    Make install distclean
+    hash -r
+}
+
+installAptLibs
+installCUDASDK
+installNvidiaSDK
+
+compileNasm
+compileYasm
+compileFontConf
+compileLibX264
+compileLibX265
+compileLibAom
+compileLibVpx
+compileLibfdkcc
+compileLibMP3Lame
+compileLibOpus
+compileLibAss
+compileFfmpeg
+
+### basicly copied from https://github.com/ilyaevseev/ffmpeg-build/blob/master/ffmpeg-nvenc-build.sh
